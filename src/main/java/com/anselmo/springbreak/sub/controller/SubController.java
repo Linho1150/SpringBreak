ea7eff401885d175776b819f0ad69be8be6c02e9
Lee Jae Jun <linho301150@gmail.com>
2022-06-19 18:05:40 +0900

    2-07 질문 목록과 템플릿 #2

--- src/main/java/com/anselmo/springbreak/sub/controller/SubController.java
+++ src/main/java/com/anselmo/springbreak/sub/controller/SubController.java
@@ -1,16 +1,33 @@
 package com.anselmo.springbreak.sub.controller;
 
+import com.anselmo.springbreak.sub.entity.SubQuestionEntity;
+import com.anselmo.springbreak.sub.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/sub")
 public class SubController {
+
+    private final QuestionRepository questionRepository;
+
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public String subMain(){
         return "hello sub";
+    }
+
+    @RequestMapping(value = "/question/list",method = RequestMethod.GET)
+    public String questionList(Model model){
+        List<SubQuestionEntity> subQuestionEntityList = this.questionRepository.findAll();
+        model.addAttribute("questionList",subQuestionEntityList);
+        return "question_list";
     }
 }
